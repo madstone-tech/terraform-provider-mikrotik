3b393a69c29bc2e99dfe9d572beee3aaabe7dbd6
Jared Gore <gore.jared@gmail.com>
2021-11-06 09:18:48 -0500

    gonna break it

---
+++ client/user.go
@@ -0,0 +1,126 @@
+package client
+
+import (
+	"fmt"
+	"log"
+)
+
+type User struct {
+	Id       string `mikrotik:".id"`
+	Address  string `mikrotik:"address"`
+	Comment  string `mikrotik:"comment"`
+	Disabled bool   `mikrotik:"disabled"`
+	Expired  bool   `mikrotik:"expired"`
+	Group    string `mikrotik:"group"`
+	Name     string `mikrotik:"name"`
+}
+
+func (client Mikrotik) AddUser(user *User) (*User, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := Marshal("/user/add", user)
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	r, err := c.RunArgs(cmd)
+
+	log.Printf("[DEBUG] ip address creation response: `%v`", r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	id := r.Done.Map["ret"]
+
+	return client.User(id)
+}
+
+func (client Mikrotik) ListUser() ([]User, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+	cmd := []string{"/user/print"}
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	r, err := c.RunArgs(cmd)
+
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("[DEBUG] found users: %v", r)
+
+	usr := []User{}
+
+	err = Unmarshal(*r, &usr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return usr, nil
+}
+
+func (client Mikrotik) FindUser(id string) (*User, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := []string{"/user/print", "?.id=" + id}
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	r, err := c.RunArgs(cmd)
+
+	log.Printf("[DEBUG] ip address response: %v", r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	usr := User{}
+	err = Unmarshal(*r, &usr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if usr.Id == "" {
+		return nil, NewNotFound(fmt.Sprintf("User `%s` not found", id))
+	}
+
+	return &usr, nil
+}
+
+func (client Mikrotik) UpdateUser(user *User) (*User, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := Marshal("/user/set", user)
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	_, err = c.RunArgs(cmd)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client.FindUser(user.Id)
+}
+
+func (client Mikrotik) DeleteUser(id string) error {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return err
+	}
+
+	cmd := []string{"/user/remove", "=.id=" + id}
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	_, err = c.RunArgs(cmd)
+	return err
+}
---
+++ mikrotik/resource_user.go
@@ -0,0 +1,147 @@
+package mikrotik
+
+import (
+	"context"
+
+	"github.com/ddelnano/terraform-provider-mikrotik/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceUser() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceIpAddressCreate,
+		ReadContext:   resourceIpAddressRead,
+		UpdateContext: resourceIpAddressUpdate,
+		DeleteContext: resourceIpAddressDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			".id": {
+				Type: schema.TypeString,
+			},
+			"address": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"comment": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"disabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"expired": {
+				Type: schema.TypeBool,
+			},
+			"group": {
+				Type: schema.TypeString,
+			},
+			"name": {
+				Type: schema.TypeString,
+			},
+		},
+	}
+}
+
+func resourceUserCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	user := prepareUser(d)
+
+	c := m.(*client.Mikrotik)
+
+	usr, err := c.AddUser(user)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return addrToData(usr, d)
+}
+
+func resourceUserRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Mikrotik)
+
+	user, err := c.FindUser(d.Id())
+
+	// Clear the state if the error represents that the resource no longer exists
+	_, resourceMissing := err.(*client.NotFound)
+	if resourceMissing && err != nil {
+		d.SetId("")
+		return nil
+	}
+
+	// Make sure all other errors are propagated
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return userToData(user, d)
+}
+
+func resourceUserUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Mikrotik)
+
+	User := prepareUser(d)
+	User.Id = d.Id()
+
+	usr, err := c.UpdateUser(User)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return userToData(usr, d)
+}
+
+func resourceUserDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Mikrotik)
+
+	err := c.DeleteUser(d.Id())
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func userToData(user *client.User, d *schema.ResourceData) diag.Diagnostics {
+	values := map[string]interface{}{
+		"address":  user.Address,
+		"comment":  user.Comment,
+		"disabled": user.Disabled,
+		"expired":  user.Expired,
+		"group":    user.Group,
+		"name":     user.Name,
+	}
+
+	d.SetId(user.Id)
+
+	var diags diag.Diagnostics
+
+	for key, value := range values {
+		if err := d.Set(key, value); err != nil {
+			diags = append(diags, diag.Errorf("failed to set %s: %v", key, err)...)
+		}
+	}
+
+	return diags
+}
+
+func prepareUser(d *schema.ResourceData) *client.User {
+	ipaddr := new(client.User)
+
+	User.Comment = d.Get("comment").(string)
+	User.Address = d.Get("address").(string)
+	User.Disabled = d.Get("disabled").(bool)
+	User.Expired = d.Get("expired").(bool)
+	User.Group = d.Get("group").(string)
+	User.Name = d.Get("name").(string)
+
+	return ipaddr
+}
